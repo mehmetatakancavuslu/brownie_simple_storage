8be9d97894f75596d3e7d512069867c657183c76
mehmetatakancavuslu <atakancavuslu322@gmail.com>
2021-12-20 17:51:58 +0300

    Connect to Rinkeby test network.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,12 +1,9 @@
-from brownie import accounts, SimpleStorage
+from brownie import accounts, SimpleStorage, network, config
 
 
 def deploy_simple_storage():
-    # Local Ganache account
-    account = accounts[0]
+    account = get_account()
     simple_storage = SimpleStorage.deploy({"from": account})
-    # Real test account with brownie encrypted
-    # account = accounts.load("test-account")
     stored_value = simple_storage.retrieve()
     print(stored_value)
     transaction = simple_storage.store(15, {"from": account})
@@ -14,6 +11,13 @@ def deploy_simple_storage():
     transaction.wait(1)
     updated_stored_value = simple_storage.retrieve()
     print(updated_stored_value)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
